2b438069a56f59fdadf7222ca50c6663ab68b2f4
sali1D <salma.elyabani@gamil.com>
2023-03-27 07:15:09 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 7-main.c 7-puts_half.c -o 7-puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts_half - a function that prints half of a string
+ * if odd len, n = (length_of_the_string - 1) / 2
+ * @str: input
+ * Return: half of input
+ */
+void puts_half(char *str)
+{
+	int a, n;
+	int count = 0;
+
+	for (a = 0; str[a] != '\0'; a++)
+		count++;
+	n = count / 2;
+	if ((count % 2) == 1)
+		n = (count + 1) / 2;
+	for (a = n; str[a] != '\0'; a++)
+		_putchar(str[a]);
+	_putchar('\n');
+}
