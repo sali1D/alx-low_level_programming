7e7c541b95fcb0d708ad6a4b036ddfd5eb2c0cf2
sali1D <salma.elyabani@gamil.com>
2023-03-29 15:04:19 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,15 +10,16 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int destlen = 0;
-	int srclen = 0;
 	int i, j;
-
+	
 	for (i = 0; dest[i] != '\0'; i++)
-		destlen++;
-	for (i = 0; src[i] != '\0'; i++)
-		srclen++;
-	for (j = 0; j < n; j++)
-		dest[destlen + j] = src[j];
+	{
+		continue;
+	}
+	for (j = 0; src[j] != '\0' && j < n; j++)
+	{
+		dest[i + j] = src[j];
+	}
+	dest[i + j] = '\0';
 	return (dest);
 }
