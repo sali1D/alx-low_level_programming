6ecafbebfe122047ead0d9937f5bb43444d2aa46
sali1D <salma.elyabani@gmail.com>
2023-05-04 12:48:30 -0700

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,13 +11,16 @@ unsigned int binary_to_uint(const char *b)
 	int i;
 	unsigned int dec_val = 0;
 
-	if (!b)
+	if (b == NULL)
 		return (0);
-	for (i = 0; b[i]; i++)
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[i] < '0' || b[i] > '1')
+		if (b[i] == '0')
+			dec_val = dec_val << 1;
+		else if (b[i] == '1')
+			dec_val = (dec_val << 1) | 1;
+		else
 			return (0);
-		dec_val = 2 * dec_val + (b[i] - '0');
 	}
 	return (dec_val);
 }
