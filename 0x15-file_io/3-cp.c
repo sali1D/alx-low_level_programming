be95e5f454a2be51f0c4b0d9ff3a99815128bc08
sali1D <salma.elyabani@gmail.com>
2023-05-09 05:13:50 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -35,7 +35,7 @@ void close_file(int fd)
 	c = close(fd);
 	if (c == 1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", c);
 		exit(100);
 	}
 }
